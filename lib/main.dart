fb7f8efaae66fde52c9ca4d3f01faf7d8be8aa3d
조성일 <chosi30629@gmail.com>
2019-12-08 21:21:39 +0900

    테마 색상 변경

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
